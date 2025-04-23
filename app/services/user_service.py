12cb9ff3cbee8cae624493abed9d636514e4153e
mikeygman11 <mgalanaugh15@gmail.com>
2025-04-23 13:00:18 -0400

    Fixed issue with 500 error when registering new user

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -66,8 +66,10 @@ class UserService:
             new_user.nickname = new_nickname
             session.add(new_user)
             await session.commit()
-            await email_service.send_verification_email(new_user)
-            
+            try:
+                await email_service.send_verification_email(new_user)
+            except Exception as e:
+                logger.warning(f"Email failed to send: {e}")            
             return new_user
         except ValidationError as e:
             logger.error(f"Validation error during user creation: {e}")
